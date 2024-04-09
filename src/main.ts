ef2958be4ea8179ac91afa0258ebea8a911890e6
Daniels <sharhan161@gmail.com>
2024-04-09 09:34:50 +0000

    updated back with fixed CORS

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,14 @@ import { PrismaService } from './prisma.service'
 import { path } from 'app-root-path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
-  
+
+
   app.setGlobalPrefix('api')
-  app.use(cookieParser())
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://94.26.225.102:3000' ],
-    credentials: true,
-    exposedHeaders: 'set-cookie'
-  })
+  app.enableCors()
   await app.listen(4200);
 }
 bootstrap();
